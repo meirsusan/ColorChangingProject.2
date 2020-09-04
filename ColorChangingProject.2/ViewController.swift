3297aca5142bc29c219b9fb5e5e205b6969b030e
meirsusan <59894052+meirsusan@users.noreply.github.com>
2020-09-04 11:58:32 +0300

    Initial Commit

---
+++ ColorChangingProject.2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ColorChangingProject.2
+//
+//  Created by Meir Susan on 4.09.2020.
+//  Copyright © 2020 Meir Susan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
